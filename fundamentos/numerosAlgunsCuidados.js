5066996fd5b31a2464f07f5c18fb09851e6e2b79
Alexandro B S <jornalsinal@gmail.com>
2025-04-06 00:00:10 -0300

    Números alguns cuidados

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0) //infinity
+console.log("10" / 2)
+console.log("10.2" / 2)
+console.log("10,2" / 2) //nan
+console.log("Show!" * 2) //nan
+console.log(0.1 + 0.7) //imprecisao por seguir especificação mais rápida, menos precisa
+//console.log(10.toString())
+console.log((10.345).toFixed(2))
