7349c78ab9dda6b46312a2e75c209b2ad993473c
Alexandro B S <jornalsinal@gmail.com>
2025-04-05 23:47:21 -0300

    Tipagem fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,8 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Evitar nome genéricos e siglas, escolha de bons nomes e prefira código claro e conciso
